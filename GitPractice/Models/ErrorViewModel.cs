7ad1a2b44b527c343ffbf234851151c07c395ff4
helia_60@yahoo.com <helia_60@yahoo.com>
2021-01-06 10:55:39 +1100

    Initial commit

---
+++ GitPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitPractice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
